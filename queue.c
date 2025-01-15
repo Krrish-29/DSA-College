aa860914de023d3d3b2e075be6adda65c3d9365e
krrish-29 <kingkrrish29@gmail.com>
2025-01-15 20:29:07 +0530

    DSA

---
+++ queue.c
@@ -0,0 +1,5 @@
+/*Enqueue: Adds an element to the end of the queue.
+Dequeue: Removes and returns the front element from the queue.
+Front/Peek: Returns the front element without removing it.
+IsEmpty: Checks if the queue is empty.
+Size: Returns the number of elements in the queue.*/
