8b2dc5010f09c8cc987aa31ddfe60c07fcb1bced
Krrish-29 <kingkrrish29@gmail.com>
2025-05-04 23:17:40 +0530

    lab

--- Practicals/113_avl_tree.c
+++ Practicals/113_avl_tree.c
@@ -1,71 +1,100 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<stdbool.h>
-typedef struct Node{
-    int data;int height;
-    struct Node*left;
-    struct Node*right;
-}Node;
-typedef struct AVL_Tree{
-    Node * root;
-}AVL_Tree;
-AVL_Tree * init_tree(){
-    AVL_Tree *Tree=(AVL_Tree*)malloc(sizeof(AVL_Tree));
-    Tree->root=NULL;
-    return Tree;}
-void inorder_traversal(Node*root){
-    if(root==NULL) return ;
-    inorder_traversal(root->left);
-    printf("%d ",root->data);
-    inorder_traversal(root->right);
+#include <stdio.h>
+#include <stdlib.h>
+typedef struct Node {
+    int data, height;struct Node *left, *right;} Node;
+typedef struct AVL_Tree {Node *root;} AVL_Tree;
+AVL_Tree* init_tree() {
+    AVL_Tree* tree = (AVL_Tree*)malloc(sizeof(AVL_Tree));
+    tree->root = NULL;return tree;}
+int getHeight(Node* root) {return (root == NULL) ? 0 : root->height;}
+int getBalanceFactor(Node* root) {return (root == NULL) ? 0 : getHeight(root->left) - getHeight(root->right);}
+Node* createNode(int data) {
+    Node* newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = data;
+    newNode->left = newNode->right = NULL;
+    newNode->height = 1;
+    return newNode;
 }
-int getHeight(Node* root) {
-    return (root == NULL) ? 0 : root->height;}
-    int getBalanceFactor(Node* root) {
-        return (root == NULL) ? 0 : getHeight(root->left) - getHeight(root->right);}
-        Node* createNode(int data) {
-            Node* newNode = (Node*)malloc(sizeof(Node));
-    newNode->data = data;newNode->left = NULL;
-    newNode->right = NULL;newNode->height = 1;
-    return newNode;}
-    Node* rotateRight(Node* root) {
-        Node* x=root->left;Node* T2=x->right;
-        x->right=root;root->left=T2;
-        root->height=1+((getHeight(root->left) > getHeight(root->right)) ? getHeight(root->left) : getHeight(root->right));
-        x->height=1+((getHeight(x->left) > getHeight(x->right)) ? getHeight(x->left) : getHeight(x->right));
-        return x;}
-        Node* rotateLeft(Node* x) {
-            Node* y=x->right;Node* T2 = y->left;
-            y->left = x;x->right = T2;
-            x->height = 1 + ((getHeight(x->left) > getHeight(x->right)) ? getHeight(x->left) : getHeight(x->right));
-            y->height = 1 + ((getHeight(y->left) > getHeight(y->right)) ? getHeight(y->left) : getHeight(y->right));
-            return y;}
-            Node* insertNode(Node* root, int data) {
-                if (root == NULL) return createNode(data);
+Node* rotateRight(Node* y) {
+    Node* x = y->left;
+    Node* T2 = x->right;
+    x->right = y;
+    y->left = T2;
+    y->height = 1 + ((getHeight(y->left) > getHeight(y->right)) ? getHeight(y->left) : getHeight(y->right));
+    x->height = 1 + ((getHeight(x->left) > getHeight(x->right)) ? getHeight(x->left) : getHeight(x->right));
+    return x;
+}
+Node* rotateLeft(Node* x) {
+    Node* y = x->right;Node* T2 = y->left;y->left = x;x->right = T2;
+    x->height = 1 + ((getHeight(x->left) > getHeight(x->right)) ? getHeight(x->left) : getHeight(x->right));
+    y->height = 1 + ((getHeight(y->left) > getHeight(y->right)) ? getHeight(y->left) : getHeight(y->right));
+    return y;}
+Node* insertNode(Node* root, int data) {
+    if (!root) {
+        printf("%d inserted into AVL Tree.\n",data);
+        return createNode(data);}
     if (data < root->data) root->left = insertNode(root->left, data);
     else if (data > root->data) root->right = insertNode(root->right, data);
     else return root;
     root->height = 1 + ((getHeight(root->left) > getHeight(root->right)) ? getHeight(root->left) : getHeight(root->right));
     int balance = getBalanceFactor(root);
-    if (balance > 1 && data < root->left->data)
-    return rotateRight(root);
-    if (balance > 1 && data > root->left->data) {
-        root->left = rotateLeft(root->left);
-        return rotateRight(root);}
-        if (balance < -1 && data > root->right->data)
-        return rotateLeft(root);if (balance < -1 && data < root->right->data) {
-            root->right = rotateRight(root->right);
-            return rotateLeft(root);}
-            return root;}
-            int main(){
-                AVL_Tree*tree=init_tree();
-                tree->root=insertNode(tree->root,7);
-                tree->root=insertNode(tree->root,5);
-                tree->root=insertNode(tree->root,8);
-                tree->root=insertNode(tree->root,2);
-    tree->root=insertNode(tree->root,4);
-    tree->root=insertNode(tree->root,3);
-    tree->root=insertNode(tree->root,1);
-    printf("Inorder traversal : ");
+    if (balance > 1 && data < root->left->data) return rotateRight(root);
+    if (balance > 1 && data > root->left->data) { root->left = rotateLeft(root->left); return rotateRight(root); }
+    if (balance < -1 && data > root->right->data) return rotateLeft(root);
+    if (balance < -1 && data < root->right->data) { root->right = rotateRight(root->right); return rotateLeft(root); }
+    return root;
+}
+Node* findMin(Node* root) {
+    while (root->left) root = root->left;
+    return root;
+}
+Node* deleteNode(Node* root, int data) {
+    if (!root) return root;
+    if (data < root->data) root->left = deleteNode(root->left, data);
+    else if (data > root->data) root->right = deleteNode(root->right, data);
+    else {
+        printf("%d is deleted form AVL Tree.\n",data);
+        if (!root->left || !root->right) {
+            Node* temp = root->left ? root->left : root->right;
+            if (!temp) { temp = root; root = NULL; }
+            else *root = *temp;
+            free(temp);
+        } else {
+            Node* temp = findMin(root->right);
+            printf("%d is replaced with %d",root->data,temp->data);
+            root->data = temp->data;
+            root->right = deleteNode(root->right, temp->data);
+        }
+    }
+    if (!root) return root;
+    root->height = 1 + ((getHeight(root->left) > getHeight(root->right)) ? getHeight(root->left) : getHeight(root->right));
+    int balance = getBalanceFactor(root);
+    if (balance > 1 && getBalanceFactor(root->left) >= 0) return rotateRight(root);
+    if (balance > 1 && getBalanceFactor(root->left) < 0) { root->left = rotateLeft(root->left); return rotateRight(root); }
+    if (balance < -1 && getBalanceFactor(root->right) <= 0) return rotateLeft(root);
+    if (balance < -1 && getBalanceFactor(root->right) > 0) { root->right = rotateRight(root->right); return rotateLeft(root); }
+    return root;
+}
+void inorder_traversal(Node* root) {
+    if (!root) return;
+    inorder_traversal(root->left);
+    printf("%d ", root->data);
+    inorder_traversal(root->right);
+}
+int main() {
+    AVL_Tree* tree = init_tree();
+    tree->root = insertNode(tree->root, 7);
+    tree->root = insertNode(tree->root, 5);
+    tree->root = insertNode(tree->root, 8);
+    tree->root = insertNode(tree->root, 2);
+    tree->root = insertNode(tree->root, 4);
+    tree->root = insertNode(tree->root, 3);
+    tree->root = insertNode(tree->root, 1);
+    printf("Inorder Traversal: ");
     inorder_traversal(tree->root);
+    printf("\n");
+    tree->root = deleteNode(tree->root, 3);
+    printf("Inorder Traversal: ");
+    inorder_traversal(tree->root);
+    printf("\n");
 }
--- Practicals/117_bfs_dfs.c
+++ Practicals/117_bfs_dfs.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include<stdbool.h>
 typedef struct Node {
     int dest;
     int weight;
     struct Node* next;
 } Node;
 typedef struct Graph {
-    Node* head[7];
+    Node* head[10];
 } Graph;
 Node* createNode(int dest, int weight) {
     Node* newNode = (Node*)malloc(sizeof(Node));
@@ -17,7 +18,7 @@ Node* createNode(int dest, int weight) {
 }
 Graph* createGraph() {
     Graph* graph = (Graph*)malloc(sizeof(Graph));
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 10; i++)
         graph->head[i] = NULL;
     return graph;
 }
@@ -30,7 +31,7 @@ void addEdge(Graph* graph, int src, int dest, int weight) {
     graph->head[dest] = newNode;
 }
 void printGraph(Graph* graph) {
-    for (int i = 0; i < 7; i++) {
+    for (int i = 0; i < 10; i++) {
         Node* temp = graph->head[i];
         printf("Node %d -> ", i);
         while (temp) {
@@ -40,20 +41,53 @@ void printGraph(Graph* graph) {
         printf("NULL\n");
     }
 }
-void breadthFirstSearch(){
+void breadthFirstSearch(Graph* graph, int start) {
+    bool visited[10] = {false};
+    int queue[10], front = 0, rear = 0;
+    visited[start] = true;
+    queue[rear++] = start;
+    printf("BFS Traversal: ");
+    while (front < rear) {
+        int current = queue[front++];
+        printf("%d ", current);
 
+        Node* temp = graph->head[current];
+        while (temp) {
+            if (!visited[temp->dest]) {
+                visited[temp->dest] = true;
+                queue[rear++] = temp->dest;
+            }
+            temp = temp->next;
+        }
+    }
+    printf("\n");
 }
-void depthFirstSearch(){
-
+void dfs(Graph* graph, int node, bool visited[]) {
+    visited[node] = true;
+    printf("%d ", node);
+    Node* temp = graph->head[node];
+    while (temp) {
+        if (!visited[temp->dest]) {
+            dfs(graph, temp->dest, visited);
+        }
+        temp = temp->next;
+    }
+}
+void depthFirstSearch(Graph* graph, int start) {
+    bool visited[10] = {false};
+    printf("DFS Traversal: ");
+    dfs(graph, start, visited);
+    printf("\n");
 }
 int main() {
     Graph* graph = createGraph();
-    addEdge(graph, 0, 1, 4);
-    addEdge(graph, 0, 2, 3);
-    addEdge(graph, 1, 2, 2);
-    addEdge(graph, 1, 3, 5);
-    addEdge(graph, 2, 3, 7);
-    addEdge(graph, 3, 4, 6);
-    addEdge(graph, 4, 5, 1);
+    addEdge(graph, 1, 4, 8);
+    addEdge(graph, 4, 7, 3);
+    addEdge(graph, 1, 2, 5);
+    addEdge(graph, 2, 5, 6);
+    addEdge(graph, 5, 9, 4);
+    addEdge(graph, 1, 3, 7);
     printGraph(graph);
+    breadthFirstSearch(graph, 1);
+    depthFirstSearch(graph, 1);
 }
