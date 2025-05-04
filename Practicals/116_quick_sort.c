52d01ce4cdf330b366ea89b27a299f7fef915250
Krrish-29 <kingkrrish29@gmail.com>
2025-05-04 13:10:34 +0530

    quick sort

---
+++ Practicals/116_quick_sort.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int partition(int array[],int left,int right){
+    int pivot=array[right];
+    int i=left-1;
+    for(int j=left;j<right;j++){
+        if(array[j]<=pivot){
+            i++;
+            int temp=array[j];
+            array[j]=array[i];
+            array[i]=temp;
+        }
+    }
+    i++;
+    int temp=array[i];
+    array[i]=array[right];
+    array[right]=temp;
+    return i;
+}
+void QuickSort(int array[],int left,int right){
+    if(left<right){
+        int pivotIndex=partition(array,left,right);
+        QuickSort(array,left,pivotIndex-1);
+        QuickSort(array,pivotIndex+1,right);
+    }
+}
+void printArray(int array[],int size){
+    for(int i=0;i<size;i++){
+        printf("%d ",array[i]);
+    }printf("\n");}
+int main(){
+    int array[]={10,9,8,7,6,5,4,3,2,1}, size=sizeof(array)/sizeof(array[0]);
+    printf("Unsorted Array: ");printArray(array,size);
+    QuickSort(array,0,size-1);
+    printf("Sorted Array: ");printArray(array,size);
+}
