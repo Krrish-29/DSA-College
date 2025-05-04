b9291a34102e2260f002c59b837cea14c4993930
Krrish-29 <kingkrrish29@gmail.com>
2025-05-05 00:02:14 +0530

    lab

--- Practicals/119_mst.c
+++ Practicals/119_mst.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include<stdbool.h>
 typedef struct Node {
     int dest;
     int weight;
@@ -39,4 +40,51 @@ void printGraph(Graph* graph) {
         }
         printf("NULL\n");
     }
+}
+Graph* prims(Graph* graph) {
+    bool inMST[7] = {false};
+    int key[7];int parent[7];
+    for (int i = 0; i < 7; i++) {
+        key[i] = INT_MAX;
+        parent[i] = -1;
+    }
+    key[0] = 0;
+    for (int count = 0; count < 7 - 1; count++) {
+        int minKey = INT_MAX, u = -1;
+        for (int v = 0; v < 7; v++) {
+            if (!inMST[v] && key[v] < minKey) {
+                minKey = key[v];
+                u = v;
+            }
+        }
+        inMST[u] = true;
+        Node* temp = graph->head[u];
+        while (temp) {
+            int v = temp->dest;
+            if (!inMST[v] && temp->weight < key[v]) {
+                key[v] = temp->weight;
+                parent[v] = u;
+            }
+            temp = temp->next;
+        }
+    }
+    Graph* mstGraph = createGraph();
+    for (int i = 1; i < 7; i++) {
+        addEdge(mstGraph, parent[i], i, key[i]);
+    }
+    return mstGraph;
+}
+int main() {
+    Graph* graph = createGraph();
+    addEdge(graph, 0, 4, 6);
+    addEdge(graph, 1, 4, 8);
+    addEdge(graph, 4, 7, 3);
+    addEdge(graph, 1, 2, 5);
+    addEdge(graph, 2, 5, 6);
+    addEdge(graph, 5, 6, 4);
+    addEdge(graph, 1, 3, 7);
+    printGraph(graph);
+    Graph* MST = prims(graph);
+    printf("Minimum Spanning Tree using Prim's Algorithm :\n");
+    printGraph(MST);
 }
