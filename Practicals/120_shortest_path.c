14fd6130e3d43e50555592f132cdb7bb44b3210c
Krrish-29 <kingkrrish29@gmail.com>
2025-05-08 22:31:32 +0530

    Djikstra algorithm

--- Practicals/120_shortest_path.c
+++ Practicals/120_shortest_path.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h> 
+#include<stdbool.h>
 typedef struct Node {
     int dest;
     int weight;
@@ -39,4 +40,48 @@ void printGraph(Graph* graph) {
         }
         printf("NULL\n");
     }
+}
+int minDistance(int distance[], bool visitedNodes[]) {
+    int min = INT_MAX, min_index;
+    for (int v = 0; v < 7; v++)
+        if (!visitedNodes[v] && distance[v] <= min)
+            min = distance[v], min_index = v;
+
+    return min_index;
+}
+void dijkstra(Graph* graph, int src) {
+    int distance[7];
+    bool visitedNodes[7];
+    for (int i = 0; i < 7; i++) {
+        distance[i] = INT_MAX;
+        visitedNodes[i] = false;
+    }
+    distance[src] = 0;
+    for (int count = 0; count < 7 - 1; count++) {
+        int u = minDistance(distance, visitedNodes);
+        visitedNodes[u] = true;
+        Node* temp = graph->head[u];
+        while (temp != NULL) {
+            int v = temp->dest;
+            if (!visitedNodes[v] && distance[u] != INT_MAX && distance[u] + temp->weight < distance[v]) {
+                distance[v] = distance[u] + temp->weight;
+            }
+            temp = temp->next;
+        }
+    }
+    printf("Vertex     Distance from %d\n", src);
+    for (int i = 0; i < 7; i++)
+        printf(" %d             %d\n", i, distance[i]);
+}
+int main() {
+    Graph* graph = createGraph();
+    addEdge(graph, 0, 4, 6);
+    addEdge(graph, 1, 4, 8);
+    addEdge(graph, 4, 7, 3);
+    addEdge(graph, 1, 2, 5);
+    addEdge(graph, 2, 5, 6);
+    addEdge(graph, 5, 6, 4);
+    addEdge(graph, 1, 3, 7);
+    printf("Graph:\n");printGraph(graph);
+    printf("\nShortest Path According to Djikstra Algorithm:\n");dijkstra(graph,0);
 }
